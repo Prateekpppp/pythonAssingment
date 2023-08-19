c9d9b6c3ec396e240a231a4699175b1352e40516
prateek <prateekpppp4@gmail.com>
2023-08-19 07:05:34 +0530

    even_odd_count

---
+++ module1/even_odd_count.py
@@ -0,0 +1,18 @@
+def count_even_odd(numbers):
+    even_count = 0
+    odd_count = 0
+    for num in numbers:
+        if num % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    return even_count, odd_count
+
+user_input = input("Enter a series of numbers separated by ',': ")
+numbers_list = list(map(int, user_input.split(',')))
+
+even_count, odd_count = count_even_odd(numbers_list)
+
+print("Number of even numbers:", even_count)
+print("Number of odd numbers:", odd_count)
+
